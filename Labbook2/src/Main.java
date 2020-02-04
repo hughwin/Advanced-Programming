ae9ead90536afe413676d5a95473e92e15afbf18
Hugh Winchester (student) <2494047W@student.gla.ac.uk>
2020-02-04 14:36:41 +0000

    Finished all tasks in exercise 1

--- Labbook2/src/Main.java
+++ Labbook2/src/Main.java
@@ -2,18 +2,26 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
-//		RunRunnable r = new RunRunnable();
-//		Thread t = new Thread(r);
-//		t.start();
-		
-		ThreadExtension[] threadExtensions = new ThreadExtension[2];
-		for(int i = 0 ; i < 2 ; i++) {
-			threadExtensions[i] = new ThreadExtension();
-			threadExtensions[i].start();
+		Thread t = new Thread();
+		for (int i = 0; i < 10; i++) {
+			RunRunnable r = new RunRunnable(" Thread " + i);
+			t = new Thread(r);
+			t.start();
+		}
+		try {
+			t.join();
+			System.out.println("Threads finished");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
+//		ThreadExtension[] threadExtensions = new ThreadExtension[2];
+//		for (int i = 0; i < 2; i++) {
+//			threadExtensions[i] = new ThreadExtension();
+//			threadExtensions[i].start();
+//		}
+//
 	}
-	
 
 }
--- Labbook2/src/RunRunnable.java
+++ Labbook2/src/RunRunnable.java
@@ -1,17 +1,18 @@
 
 public class RunRunnable implements Runnable{
 	
-	public RunRunnable() {
-		
+	private String name;
+
+	public RunRunnable(String name) {
+		this.name = name;
 	}
 
 	@Override
 	public void run() {
 		try {
 			Thread.sleep(1000);
-			System.out.print("Hello");
-			Thread.sleep(1000);
-			System.out.print(" Hugh");
+			System.out.println(name);
+
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
