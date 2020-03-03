0f278e46eb15d45883b7cab7f9770a5e8b9a5603
hughwin <hugh.winchester@gmail.com>
2020-03-03 09:06:12 +0000

    All quotes loaded from server.

--- Labbook3/src/server/Client.java
+++ Labbook3/src/server/Client.java
@@ -3,16 +3,21 @@ package server;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private static int PORT = 1234;
     private static String server = "127.0.0.1";
     public Client() {
+        Socket socket = null;
         try {
-            Socket socket = new Socket(server, PORT);
-            InputStreamReader sr = new InputStreamReader(socket.getInputStream());
-            int c = sr.read();
-            System.out.println((char)c);
+            socket = new Socket(server, PORT);
+
+            Scanner sr = new Scanner(socket.getInputStream());
+            while(sr.hasNextLine()){
+                System.out.println(sr.nextLine());
+            }
+            sr.close();
             socket.close();
 
         }catch (IOException e){
--- Labbook3/src/server/Server.java
+++ Labbook3/src/server/Server.java
@@ -1,9 +1,13 @@
 package server;
 
+import javax.imageio.IIOException;
 import java.io.DataInput;
+import java.io.File;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server implements Runnable {
     private static int PORT = 1234;
@@ -14,18 +18,32 @@ public class Server implements Runnable {
     @Override
     public void run() {
         while(true){
-            try{ ServerSocket listener = new ServerSocket(PORT);
-                Socket client = listener.accept();
+            ServerSocket listener = null;
+            Socket client = null;
+
+            try {
+                listener = new ServerSocket(PORT);
+                client = listener.accept();
                 System.out.println("Client has arrived");
 
                 OutputStreamWriter os = new OutputStreamWriter(client.getOutputStream());
 
-                os.write('x');
-                os.flush();
 
+                File file = new File("Labbook3/src/server/quotes.txt");
+                Scanner textInput = new Scanner(file);
+
+                while (textInput.hasNextLine()) {
+                    String line = textInput.nextLine();
+                    if (line.equals("END")) {
+                        break;
+                    }
+                    os.write(line + "\n");
+                    os.flush();
+                }
+                textInput.close();
                 client.close();
                 listener.close();
-            } catch(Exception e){
+            }catch (IOException e){
                 e.printStackTrace();
             }
         }
